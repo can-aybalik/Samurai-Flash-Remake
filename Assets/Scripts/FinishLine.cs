d0c6bff929c060c8760d8daacb7c684c1cef25d4
Can Aybalık <can.moonfish@gmail.com>
2020-07-30 16:47:43 +0300

    Finish Line is added

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("FINISH!");
+        }
+
+    }
+}
